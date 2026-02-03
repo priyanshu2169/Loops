28c3fff665e09cf6a355d9aef657a3c17c0a081d
priyanshu2169 <priyanshutyagi0910@gmail.com>
2026-02-03 23:43:46 +0530

    reverse a given number

---
+++ reverseNumber2.java
@@ -0,0 +1,13 @@
+
+public class reverseNumber2{
+    public static void main(String args[]){
+        int n=854678;
+        int rev=0;
+        while(n>0){
+            int last_digit=n%10;
+            rev=(rev*10)+last_digit;
+            n=n/10;
+        }
+        System.out.println(rev);
+    }
+}
