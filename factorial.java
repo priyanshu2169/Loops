0008bc48d9802d67e8d11681e47089e566895c2f
priyanshu2169 <priyanshutyagi0910@gmail.com>
2026-02-05 01:22:37 +0530

    factorial

---
+++ factorial.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class factorial {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int fact=1;
+        while(n>0){
+            if(n==0){
+                fact=1;
+            }else{
+            fact=fact*n;
+            n--;
+            }
+        }
+        System.out.println(fact);
+        sc.close();
+    }
+}
